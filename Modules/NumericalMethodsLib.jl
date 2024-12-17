cbffb411a9d56c4aa98f859ea5cdbff491293c7a
Git-User-JW <112542731+Git-User-JW@users.noreply.github.com>
2024-12-18 09:37:43 +1300

    Learn

---
+++ Modules/NumericalMethodsLib.jl
@@ -0,0 +1,12 @@
+module NumericalMethodsLib
+
+# ---- imports ----
+using Integrals
+
+# ---- include ----
+include("FourierAnalysis.jl")
+
+# ---- exports ----
+export FS_analysis, FS_synthesis
+
+end
